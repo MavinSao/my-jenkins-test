db50d3f58b69f10c15db012be4dc329777fe1b94
Mavin Sao <mavinsao@Mavins-Mac-mini.local>
2022-03-31 11:04:10 +0700

    update Link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Mavin
+          Learn React With Mavin OK?
         </a>
       </header>
     </div>
