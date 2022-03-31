cf4f2e5baf17e75f240c79672a499bde684bd244
Mavin Sao <mavinsao@Mavins-Mac-mini.local>
2022-03-31 11:12:55 +0700

    change text

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My New Up Date
-        </a>
-      </header>
+      <h1>Is it ok?</h1>
     </div>
   );
 }
