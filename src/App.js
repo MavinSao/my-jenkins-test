3aeca5a787a4e81a847c265e131837041dc955ec
Mavin Sao <mavinsao@Mavins-Mac-mini.local>
2022-03-31 11:06:18 +0700

    update label

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Mavin OK?
+          My New Up Date
         </a>
       </header>
     </div>
